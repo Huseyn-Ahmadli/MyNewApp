c478fb6a02b539250a137474ea829fa456046a03
Huseyn Ahmadli <huseyn.ahmadli@digit.az>
2022-01-09 18:44:38 +0400

    fixup!fixup!fixup!fixup!

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :find_question!
 
   def create
@@ -20,7 +21,7 @@ class AnswersController < ApplicationController
     @answer = @question.answers.find(params[:id])
     if @answer.update(answer_params)
       flash[:success] = 'Answer updated!'
-      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
+      redirect_to question_path(@question, anchor: dom_id(@answer))
     else
       render :edit
 
